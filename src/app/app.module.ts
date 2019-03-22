ab619a4e744da2ce818405ee7ab0c4cfefd2cea4
Rajan Subedi <rajan.subedi32@gmail.com>
2019-03-22 12:44:43 +0530

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {  CoursesModule } from './courses/courses.module';
+import { StaticpagesModule } from './staticpages/staticpages.module';
+import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoursesModule,
+    StaticpagesModule,
+    AdminModule,
+    AuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
